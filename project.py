f78b9374362307a0293a76ebaca866f3597fea58
Aamir Khan <aamir.k56@gmail.com>
2017-02-21 13:25:24 +0530

    Rendered login.html in login() function

--- project.py
+++ project.py
@@ -29,7 +29,7 @@ def showLogin():
                                   string.digits)
                     for x in range(32))
     login_session['state'] = state
-    return "The current state is %s" %login_session['state']
+    return render_template('login.html', STATE = state)
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
